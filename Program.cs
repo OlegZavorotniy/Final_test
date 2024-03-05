a5d72094b7716ebae0c964f74dcd4ca3969c4436
Oleg Zavorotniy <o.zavorotniy@hotmail.com>
2024-03-05 23:43:18 +0300

    add first method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+string [] CreateNewMassive(string [] str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        Console.WriteLine("Введите строку: ");
+        string newString = Console.ReadLine()!;
+        str[i] = newString;
+    }
+    return str;
+}
